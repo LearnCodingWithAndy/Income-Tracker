c013b57b3b0bb141f967efb98fca1acafedf9a33
Anand Kulkarni <learncodingwithandy@gmail.com>
2021-02-12 17:03:58 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+/* Components*/ 
+import HomeScreen from './src/Components/HomeScreen';
+import AddTransaction from './src/Components/AddTransaction';
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Add" component={AddTransaction} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/Components/AddTransaction.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddTransaction = () => {
+    return (
+        <View>
+            <Text>
+                Add Transaction
+            </Text>
+        </View>
+    )
+}
+
+export default AddTransaction;
